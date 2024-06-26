aa764444b0d45c5db5bd6834b579e117049ecccf
Lindsay Terry <lindsaytee66@gmail.com>
2024-06-26 13:21:58 -0600

    Created html route to pull up notes page

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+//Import route files
+const api = require('./routes/api');
+const html = require('./routes/serve-html');
 
 const app = express();
 const PORT = 3001;
@@ -6,6 +9,12 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded( {extended: true }));
 app.use(express.static('public'));
+
+//sets up route handling
+// app.use('/api', api);
+app.use('/', html);
+
+
 
 app.listen(PORT, () => 
     console.log(`Listening on port ${PORT}`));
