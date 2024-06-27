562412b6d24d3baf3786d6e7e312af7b86ad55b9
Lindsay Terry <lindsaytee66@gmail.com>
2024-06-27 14:29:14 -0600

    Function to write newly added notes to db file

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,13 @@
 const api = require('express').Router();
 const path = require('path');
 const notes = require('../db/db.json');
+const savedNotes = require('../helpers/saveNotes');
+const fs = require ('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
 
 //request to retrieve notes from DB
 api.get('/notes', (req, res) => {
@@ -12,10 +19,33 @@ api.post('/notes', async (req, res) => {
     const { title, text } = req.body;
 
     if (title && text ) {
-        const new Note {
-            
+            const newNote = {
+                title,
+                text,
+            };
+
+            try {
+                const data = await readFile('./db/db.json', 'utf8');
+                const notes = JSON.parse(data);
+                notes.push(newNote);
+
+                await writeFile('./db/db.json', JSON.stringify(notes, null, 4));
+                console.log('Note successfully recorded');
+
+                // await readFile('./db/db.json', 'utf-8');
+
+                res.json(api.get('/notes', (req, res) => {
+                    return notes;
+                }));
+            } catch (error) {
+                console.log('Error, try again');
+            } 
+        } else {
+            res.status(400).json({error: 'Title and text required'});
         }
     }
+)
+module.exports = api;
 
     //     console.log(req.body);
     //     notes.push(req.body);
@@ -25,6 +55,3 @@ api.post('/notes', async (req, res) => {
     //     // console.info(`${req.method} request received to get notes`);
     //     return res.status(200).json(notes);
     // })
-}
-)
-module.exports = api;
