a016c787816d0ab060acf683462ac646525a3ee6
Lindsay Terry <lindsaytee66@gmail.com>
2024-06-27 17:16:13 -0600

    Modularized logic to post new note

---
+++ helpers/readWrite.js
@@ -0,0 +1,36 @@
+const notes = require('../db/db.json');
+
+const fs = require ('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+//function to create a new note and assign title and text to req.body
+const createNote = async (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+        title,
+        text,
+    };
+
+    //reads the db file with all stored notes
+    try {
+        const data = await readFile('./db/db.json', 'utf8');
+        // const savedNotes = JSON.parse(data); //do i need this?
+        notes.push(newNote);
+
+        //writes db file with updated notes
+        await writeFile('./db/db.json', JSON.stringify(notes, null, 4));
+        res.status(201).json(notes);
+    } catch (error) {
+        console.log('Error, try again');
+    }
+  } else {
+    res.status(400).json({error: 'Title and text required'});
+  }
+}
+
+module.exports = createNote;
--- routes/api.js
+++ routes/api.js
@@ -1,54 +1,12 @@
 const api = require('express').Router();
-const path = require('path');
+const createNote = require('../helpers/readWrite');
 const notes = require('../db/db.json');
-const savedNotes = require('../helpers/saveNotes');
-const fs = require ('fs');
-const util = require('util');
-
-const readFile = util.promisify(fs.readFile);
-const writeFile = util.promisify(fs.writeFile);
-
 
 //request to retrieve notes from DB
 api.get('/notes', (req, res) => {
-    // console.info(`${req.method} request received to get notes`);
     return res.status(200).json(notes);
 })
 
-api.post('/notes', async (req, res) => {
-    const { title, text } = req.body;
-
-    if (title && text ) {
-            const newNote = {
-                title,
-                text,
-            };
-
-            try {
-                const data = await readFile('./db/db.json', 'utf8');
-                const savedNotes = JSON.parse(data);
-                notes.push(newNote);
-
-                await writeFile('./db/db.json', JSON.stringify(savedNotes, null, 4));
-                console.log('Note successfully recorded');
-
-                res.status(201).json(savedNotes);
-
-            } catch (error) {
-                console.log('Error, try again');
-            } 
-        } else {
-            res.status(400).json({error: 'Title and text required'});
-        }
-    }
-)
+//request to post notes to db
+api.post('/notes', createNote);
 module.exports = api;
-
-    //     console.log(req.body);
-    //     notes.push(req.body);
-    //     console.log(notes); 
-
-    // api.get('/notes', (req, res) => {
-    //     // console.info(`${req.method} request received to get notes`);
-    //     return res.status(200).json(notes);
-    // })
